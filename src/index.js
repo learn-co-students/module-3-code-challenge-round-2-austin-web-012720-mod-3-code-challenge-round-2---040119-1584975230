f1eb133ad7d14f43dcefba516964cd6eb98e93c8
jelenastj <jelena.stjepanovic@gmail.com>
2020-03-23 11:45:38 -0500

    code ch2

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//I think I'd know how to do this challenge(it's similar to Bookliker). But I am so emotionally and physically exhausted by this program,
+//(in addition to the remote setting), that I have zero energy to do it. 
+//And I feel ashamed that I broke down in front of 20 people during the zoom stand-up this morning.
+//Still not sure how this whole experience is preparing us for a job enviroment if during the process
+//we are experiencing so much anguish. I am not the only one who feels this way in this cohort, 
+//so it's not only the issue of asking or not asking for help. I worked in stressfull enviroments,
+// with tight deadlines and long hours over a decade, and I was always able to deliver. 
+// And I feel like throughout all my 1-1s I was never trully heard, or properly assesed, 
+//and that my experiece has been too easily dissmissed as imaginary and/or common. 
+//I've never imagined I'll be experiencing this, and I've never been so exposed in my life trying 
+//only to do the best I can.
