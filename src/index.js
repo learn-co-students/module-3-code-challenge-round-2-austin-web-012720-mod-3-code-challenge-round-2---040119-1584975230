570f823a4d32cf1917817a32665398e64bb634ec
lughjw <lughjw@gmail.com>
2020-03-23 11:24:59 -0500

    step 3 done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 const BASE_URL = 'http://localhost:3000'
 const BEERS_URL = `${BASE_URL}/beers`
 
+let beerList = [];
+
 document.addEventListener('DOMContentLoaded', setup)
 
 function setup(e) {
@@ -12,7 +14,23 @@ function setup(e) {
 function fetchBeerList() {
     fetch(BEERS_URL)
         .then(resp => resp.json())
-        .then(beerList => displayBeerList(beerList))
+        .then(jsonBeerList => {
+            beerList = jsonBeerList; 
+            displayBeerList(beerList)
+        });
+}
+
+function patchBeer(beerId, newDescription) {
+    // console.log("patch ", beerId, newDescription)
+    fetch(`${BEERS_URL}/${beerId}`,{
+        method: 'PATCH',
+        headers: {
+            'Content-type' : 'application/json',
+            'Accept' : 'application/json'
+        },
+        body: JSON.stringify({description: newDescription})
+    })
+    
 }
 
 // function fetchBeer(beerId) {
@@ -29,10 +47,20 @@ function displayBeerList(beerList) {
 }
 
 
-
 // function displayBeer(beer) {
     
 // }
+
+/* Button handlers */
+// function handleSaveBeerDescription(e) {
+//     // e.preventDefault();
+
+//     patchBeer(el('beer-descpription'))
+
+//     displayBeer()
+// }
+
+/* Make DOM elements */
 
 function makeBeerNameLi(beer) {
     const beerLi = document.createElement('li');
@@ -61,9 +89,16 @@ function makeBeerEl(beer) {
     imageEl.src = beer.image_url;
     taglineEl.innerText = beer.tagline;
     descriptionArea.innerText = beer.description;
+    descriptionArea.id = 'beer-description';
     buttonEl.id = 'edit-beer';
     buttonEl.class = 'btn btn-info';
     buttonEl.innerText = 'Save';
+    buttonEl.addEventListener('click', e => {
+        // console.log("Value: ", descriptionArea.value)
+        // console.log('innerText: ', descriptionArea.innerText)
+        beerList[beer.id].description = descriptionArea.value;
+        patchBeer(beer.id,descriptionArea.value);
+    });
 
     beerDiv.appendChild(nameEl);
     beerDiv.appendChild(imageEl);
